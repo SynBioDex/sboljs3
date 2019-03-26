83690c809b26b2c1a8896c659b231d0e5e693099
James Alastair McLaughlin <james@mclgh.net>
2019-03-26 11:02:34 -0700

    fix facadeType for S2ExperimentalData

--- sbolgraph/sbol2/S2ExperimentalData.ts
+++ sbolgraph/sbol2/S2ExperimentalData.ts
@@ -10,7 +10,7 @@ export default class S2ExperimentalData extends S2Identified {
     }
 
     get facadeType():string {
-        return Types.SBOL2.Experiment
+        return Types.SBOL2.ExperimentalData
     }
 
 }
