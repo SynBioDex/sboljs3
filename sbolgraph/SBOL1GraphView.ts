0a3335b4ae19eebab529216192d587d678bba210
James McLaughlin <james@mclgh.net>
2020-05-10 21:26:16 +0100

    0.18.0

--- sbolgraph/SBOL1GraphView.ts
+++ sbolgraph/SBOL1GraphView.ts
@@ -1,10 +1,10 @@
 
-import { Graph, GraphViewBasic, Facade } from 'rdfoo'
+import { Graph, GraphViewBasic, Facade, serialize } from 'rdfoo'
 import S1DnaSequence from './sbol1/S1DnaSequence'
 import S1DnaComponent from './sbol1/S1DnaComponent'
 import S1Collection from './sbol1/S1Collection'
 import S1SequenceAnnotation from './sbol1/S1SequenceAnnotation'
-import { Types } from 'bioterms'
+import { Types, Prefixes, Predicates } from 'bioterms'
 import S1Facade from './sbol1/S1Facade'
 
 export default class SBOL1GraphView extends GraphViewBasic {
@@ -70,6 +70,47 @@ export default class SBOL1GraphView extends GraphViewBasic {
         return topLevels.map((topLevel) => this.uriToFacade(topLevel) as S1Facade)
     }
 
+    serializeXML() {
+
+        let defaultPrefixes:Array<[string,string]> = [
+            [ 'rdf', Prefixes.rdf ],
+            [ 'dcterms', Prefixes.dcterms ],
+            [ 'prov', Prefixes.prov ],
+            [ 'sbol', Prefixes.sbol2 ],
+            [ 'sbol3', Prefixes.sbol3 ],
+            [ 'backport', 'http://biocad.io/terms/backport#' ],
+            [ 'om', Prefixes.measure ],
+        ]
+
+        let ownershipPredicates = [
+            Predicates.SBOL1.subComponent,
+            Predicates.SBOL1.annotation
+        ]
+
+        let isOwnershipRelation = (triple:any):boolean => {
+
+            let p = nodeToURI(triple.predicate)
+
+            if(ownershipPredicates.indexOf(p) !== -1) {
+                return true
+            }
+
+            return false
+        }
+
+        return serialize(this.graph, new Map(defaultPrefixes), isOwnershipRelation)
+
+        function nodeToURI(node):string {
+
+            if(node.interfaceName !== 'NamedNode')
+                throw new Error('expected NamedNode but found ' + JSON.stringify(node))
+
+            if(typeof node.nominalValue !== 'string')
+                throw new Error('nominalValue not a string?')
+
+            return node.nominalValue
+        }
+    }
 }
 
 export function sbol1(graph:Graph) {
